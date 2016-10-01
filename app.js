b224ab75a929680356f250cc0a9c788a9eda4942
nickletgo <nickletgo@gmail.com>
2016-10-01 13:23:25 -0400

    Print request params to console

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const port = process.env.PORT || 3000
 
 app.get('/', function (req, res) {
     console.log('get method:')
+    console.log(JSON.stringify(req.params))
     res.send(JSON.stringify(req.params))
 })
 
 app.post('/', (req, res) => {
     console.log('post method')
+    console.log(JSON.stringify(req.params))
     res.send(JSON.stringify(req.params))
 })
 
