a90ac0e61290d4c595b66c9fc78f66f1b02ca90c
nickletgo <nickletgo@gmail.com>
2016-10-01 13:18:15 -0400

    Added code to print params from request

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', function (req, res) {
-    res.send('Hello World!')
+    console.log('get method:')
+    res.send(JSON.stringify(req.params))
+})
+
+app.post('/', (req, res) => {
+    console.log('post method')
+    res.send(JSON.stringify(req.params))
 })
 
 app.listen(port, function () {
-    console.log(`Example app listening on port $(port)!`)
+    console.log(`Example app listening on port ${port}!`)
 })
